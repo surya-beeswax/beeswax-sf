1b00cb95c84a9b5c265d3734eaf65962e387d206
bridge_api_integration <75660548+surya-beeswax@users.noreply.github.com>
2020-12-08 12:36:26 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,7 @@
+username = 'arun.arumugam@mediamint.com'
+password = 'Vidhura@101624@'
+# isSandbox = 1
+domain = 'test'
+token = 'pcLbyaEgzvYAgZDe8MOOh5jz'
+instance = 'cs21.salesforce.com'
+client_id = 'sf_rest_api'
